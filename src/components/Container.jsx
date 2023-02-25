828dfc880068bcc0759ee7ff099f091ef6bb66e1
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-02-25 15:28:19 -0400

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Card from "./Card";
 import Player from "./Player";
 
 function Container() {
-    return(
-        <div class="container" style={{"marginTop":"2%"  ,"background-color": "#e3f2fd"}}>
-            <Player />
+    return (
+        <div className="container text-center">
+            <div className="Card" style={{ "marginTop": "2%", "background-color": "#e3f2fd" }}>
+                <div className="row">
+                    <div className="col">
+                        <Player />
+                    </div>
+                    <div className="col">
+                        <Card />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
