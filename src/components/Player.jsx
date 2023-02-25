555b8739006344e54cde74e1785ab2aa8ca25863
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-02-25 15:28:22 -0400

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,46 +1,52 @@
 import React from "react";
-import './Player.jsx.scss'
+import './Player.jsx.scss';
+
+//import { Slider } from 'primereact/slider';
+        
 
 function Player() {
+    //const [range, setRange] = 0;
+
     return (
-        <div class="wrapper">
-            <div class="player__container">
-                <div class="player__body">
-                    <div class="body__cover">
+        <div className="wrapper">
+            <div className="player__container">
+                <div className="player__body">
+                    <div className="body__cover">
                         <img src="http://ecx.images-amazon.com/images/I/51XSHShbPiL.jpg" alt="Album cover" />
 
-                        <div class="range"></div>
+                        <div className="range"></div>
+                        {/* <Slider value={range} onChange={(e) => setRange(e.value)} />  */}
                     </div>
 
-                    <div class="body__info">
-                        <div class="info__album">The Hunting Party</div>
+                    <div className="body__info">
+                        <div className="info__album">The Hunting Party</div>
 
-                        <div class="info__song">Final Masquerade</div>
+                        <div className="info__song">Final Masquerade</div>
 
-                        <div class="info__artist">Linkin Park</div>
+                        <div className="info__artist">Linkin Park</div>
                     </div>
 
-                    <div class="body__buttons">
-                        <ul class="list list--buttons">
+                    <div className="body__buttons">
+                        <ul className="list list--buttons">
                             <li>
-                                <a href="/" class="list__link">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                                <a href="/" className="list__link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
                                         <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
                                     </svg>
                                 </a>
                             </li>
 
                             <li>
-                                <a href="/" class="list__link">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                                <a href="/" className="list__link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
                                         <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
                                     </svg>
                                 </a>
                             </li>
 
                             <li>
-                                <a href="/" class="list__link">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <a href="/" className="list__link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
                                         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
                                     </svg>
                                 </a>
@@ -49,16 +55,8 @@ function Player() {
                     </div>
                 </div>
 
-                <div class="player__footer">
-                    <ul class="list list--footer">
-                        {/*  <li><a href="#" class="list__link"><i class="fa fa-heart-o"></i></a></li>
-
-                        <li><a href="#" class="list__link"><i class="fa fa-random"></i></a></li>
-
-                        <li><a href="#" class="list__link"><i class="fa fa-undo"></i></a></li>
-
-                        <li><a href="#" class="list__link"><i class="fa fa-ellipsis-h"></i></a></li> */}
-                    </ul>
+                <div className="player__footer">
+                    
                 </div>
             </div>
         </div>
