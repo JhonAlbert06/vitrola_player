5b4cde564a9c909552c8db1f9e8e7aff9dc044dc
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-02-25 15:28:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import Container from './components/Container';
 import Navbar from './components/Navbar';
 
 
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";                                         
+        
+
+
 function App() {
   return (
     <div className="App">
