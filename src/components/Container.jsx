3277f64466eee1612dc4b3cb71feea657f80b43e
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2023-02-25 12:22:17 -0400

    Create Container.jsx

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Player from "./Player";
+
+function Container() {
+    return(
+        <div class="container" style={{"marginTop":"2%"  ,"background-color": "#e3f2fd"}}>
+            <Player />
+        </div>
+    );
+}
+
+export default Container;
